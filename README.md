6079671229a53c4162c9ea06d07882f3bef7014a
bbest <bbest@nceas.ucsb.edu>
2014-06-17 14:29:48 -0700

    finished ohi-science/ohicore#105

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 ohi-uswest
 ==========
 
-Layers for Halpern et al (2014) Assessing the health of the U.S. west coast with a regional-scale application of the Ocean Health Index. _PLOS One_.
+This repository contains data from:
+
+Halpern et al (2014) Assessing the health of the U.S. west coast with a regional-scale application of the Ocean Health Index. _PLOS One_.
+
+The folders correspond to:
+
+- **conf**: configuration files
+- **layers**: input data used to calculate the dimensions (pressures, resilience, status and trend) of each goal
+- **scores**: output scores
+- **spatial**: vector (shapefile) and raster (GeoTIFF) formats of the regions used
+
+For more information, see:
+
+- [OceanHealthIndex.org](oceanhealthindex.org): general public website presenting results
+- [OHI-Science.org](ohi-science.org): scientific tools and data for calculating OHI
--- create.R
+++ create.R
@@ -1,32 +1,36 @@
-library(RPostgreSQL)
 library(reshape2)
 library(plyr)
 library(stringr)
 library(dplyr)
+library(gdata)
 
-
-# working directory
+# get machine paths
 source('../ohiprep/src/R/common.R') # get dir_neptune_data
 
-dir.create('layers', showWarnings=F)
+# create directories
+for (dir in c('conf','layers','scores','spatial')) dir.create(dir, showWarnings=F)
 
-# connect to postgres db
-dbi.pg = dbDriver('PostgreSQL') 
-pg = dbConnect(dbi.pg, host='neptune.nceas.ucsb.edu', dbname='ohi_cc2012', user='bbest') # assumes password in ~/.pgpass
+# connect to postgres db: not used yet
+#library(RPostgreSQL)
+#dbi.pg = dbDriver('PostgreSQL') 
+#pg = dbConnect(dbi.pg, host='neptune.nceas.ucsb.edu', dbname='ohi_cc2012', user='bbest') # assumes password in ~/.pgpass
 #dbSendQuery(pg, 'SET search_path TO global_li, global; SET ROLE TO ohi;')
 
-# conf: goal weights, derived pressures & resilience in long form
-dir.create('conf', showWarnings=F)
-dir_data = file.path(dir_neptune_local, 'src/model/california-current/data')
-for (f in c('cc_pressures.csv','cc_resilience.csv','goal_weights.csv')){
+# conf: goal weights
+for (f in c('goal_weights.csv')){
   file.copy(
-    file.path(dir_data, f),
+    file.path(dir_neptune_local, 'src/model/california-current/data', f),
     file.path('conf', str_replace(f, fixed('cc_'),'')))
 }
 
-# conf: derived scores of different scenarios
-dir.create('scores', showWarnings=F)
-dir_doc = file.path(dir_neptune_local, 'src/model/california-current/doc')
+# derived pressures & resilience in long form
+for (f in c('cc_pressures.csv','cc_resilience.csv')){
+  file.copy(
+    file.path(dir_neptune_local, 'src/model/california-current/data', f),
+    file.path('layers', str_replace(f, fixed('cc_'),'')))
+}
+
+# scores
 files = c(
   'scores_index.csv'='results_cc_scores.csv', 'scores_dimension.csv'='results_cc_data.csv',
   setNames(
@@ -36,24 +40,21 @@ files = c(
     sprintf('results_cc_data_scenario%s.csv'       , c('1','2','3a','3b','3c')), 
     sprintf('scores_dimension_scenario%s.csv', c('1','2','3a','3b','3c'))))
 for (i in 1:length(files)){ # i=1
-  #print(f)
-  #print(head(read.csv(file.path(dir_doc, f))))
   f_from = files[[i]]
   f_to   = names(files)[[i]]
   file.copy(
-     file.path(dir_doc, f_from),
+     file.path(dir_neptune_local, 'src/model/california-current/doc', f_from),
      file.path('scores', f_to))
 }
     
 # regions layers
 for (f in c('cc_regions.csv','cc_regions_area.csv','cc_regions_counties.csv')){
   file.copy(
-    file.path(dir_data, f),
+    file.path(dir_neptune_local, 'src/model/california-current/data', f),
     file.path('layers', str_replace(f, fixed('cc_'),'')))
 }
 
 # regions spatial
-dir.create('spatial', showWarnings=F)
 dir_regions = file.path(dir_neptune_local, 'src/model/california-current/data/regions')
 for (f in list.files(dir_regions)){
   file.copy(
@@ -68,7 +69,7 @@ for (v in names(cc_ao_data)){ # v = names(cc_ao_data)[1]
   write.csv(cc_ao_data[[v]], csv, na='', row.names=F)
 }
 
-# HAB, CP
+# HAB, CP, CS
 load(file.path(dir_neptune_data, 'model/CC-NCEAS-Habitats/data/cc_habitat_model.RData'))
 write.csv(cc_habitat_model, 'layers/hab_metrics.csv', na='', row.names=F)
 
@@ -83,7 +84,6 @@ file.copy(
   'layers/fis_data.csv')
 
 # MAR
-cat('loading MAR harvest...\n')
 file.copy(
   file.path(dir_neptune_data, 'stable/CC-NCEAS-FP/data/Dumbauld_etal_2009_farmable_bay_area.csv'),
   'layers/mar_farmable_area.csv')
@@ -97,3 +97,35 @@ write.csv(
     species=names(d), 
     sustainability=d), 
   'layers/mar_sustainability.csv', na='', row.names=F)
+
+# ICO
+load(file.path(dir_neptune_local, 'src/model/california-current/ICO/data/ico_data.RData'))
+write.csv(melt(ico_data), 'layers/ico_species.csv', row.names=F, na='')
+
+# LE
+load(file.path(dir_neptune_local, 'src/model/california-current/LE/status/data/cc_le_status_model_data.Rdata'))
+write.csv(melt(cc_le_status_model_data[['data']]), 'layers/le_data.csv', row.names=F, na='')
+write.csv(melt(cc_le_status_model_data[['adjustments']]), 'layers/le_adjustments.csv', row.names=F, na='')
+d = melt(cc_le_status_model_data[['le.counties']])
+names(d) = c('county','region')
+write.csv(d, 'layers/le_counties.csv', row.names=F, na='')
+load(file.path(dir_neptune_local, 'src/model/california-current/LE/trend/data/cc_le_trend_model_data.Rdata'))
+write.csv(
+  rename(melt(cc_le_trend_model_data[['weights']]), c('L1'='metric')), 
+  'layers/le_weights.csv', row.names=F, na='')
+
+# LSP
+file.copy(
+  file.path(dir_neptune_data, 'model/CC-NCEAS-LSP/data/lsp_model_data.csv'),
+  'layers/lsp_data.csv')
+
+# SPP
+file.copy(
+  file.path(dir_neptune_data, 'model/CC-NCEAS-Species/data/cc_spp_results.csv'),
+  'layers/spp_data.csv')
+
+# TR
+load(file.path(dir_neptune_data, 'model/CC-NCEAS-TR/data/tr_results.RData'))
+write.csv(
+  tr_results[['prediction']],
+  'layers/tr_data.csv', row.names=F, na='')
